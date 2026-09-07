45126ecd4838f3c37762a918a2229f54cebb1530
Tushar Mishra <tusharmishra587@gmail.com>
2026-09-07 23:41:05 +0530

    DSA DAY 21

---
+++ Day_21/linked.java
@@ -0,0 +1,69 @@
+/* roblem: Create and Traverse Singly Linked List
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the result
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50*/
+package Day_21;
+import java.util.*;
+
+class Main {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        Node head = null;
+        Node tail = null;
+
+        // Create linked list
+        for (int i = 0; i < n; i++) {
+
+            int value = sc.nextInt();
+            Node newNode = new Node(value);
+
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        // Traverse linked list
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data);
+
+            if (temp.next != null) {
+                System.out.print(" ");
+            }
+
+            temp = temp.next;
+        }
+    }
+}
